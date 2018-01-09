ebd781bc6e63dae5228540c335ccea57cdd347e8
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2018-01-09 16:58:53 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,19 @@
+#iclude<stdio.h>
+void main()
+{
+int a=10;
+int b=20;
+int c=30;
+if(a>b)&&(a>c)
+{
+printf("a is big");
+}
+if else(b>c)&&(b>a)
+{
+printf("b is big");
+}
+else
+{
+printf("c is big"):
+}
+}
