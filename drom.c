ed2485f81a1a3d0c9a569961f6796b3a63d767fa
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2018-01-19 08:58:56 +0000

    Create drom.c

---
+++ drom.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a,r=0,n,s;
+printf("rnter the number");
+scanf("%d",&a);
+while(n>0)
+{
+s=n%10;
+r=r+s*s*s;
+if
+}
+printf("yes"
+}
+else
+{
+printf("no");
+}
