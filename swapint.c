92d4a5f5c413ec23904db1eddc7720b4b4b5135f
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2018-01-21 10:37:41 +0530

    Create swapint.c

---
+++ swapint.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int siz,a1[10],i,j,temp;
+printf("enter the limit,enter the number");
+scanf("%d",&siz);
+for(i=0;i<=siz;i++)
+{
+scanf("%d",&a1[10]);
+}
+for(i=0;i<=siz;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+if(a[i]<a[j])
+{
+temp=a[i];
+}
+else
+{
+temp=[j];
+printf(%d",a[j]);
+}
+}
