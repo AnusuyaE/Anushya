f4dd943a82e7d6c9082304590661902b921f195a
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2018-01-21 09:21:24 +0530

    Create evenint.c

---
+++ evenint.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int i,f,l;
+printf("enter the lst reversal");
+scanf("%d",&l);
+for(i=f;i<=1;i++)
+if((i%2)==0)
+{
+printf("the number is even");
+}
+else
+{
+printf("the number is odd");
+}
+return 0;
+}
+}
