be485e6c45e929ceecdd6ab4427bf31f4352beb1
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2018-01-21 09:29:42 +0530

    Create oddint.c

---
+++ oddint.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int i,f,l;
+printf("enter the l reversal");
+scanf("%d",&l);
+for(i=f;i<=l;i++)
+{
+if((i%3)==0)
+{
+printf("the number is odd");
+}
+else
+{
+printf("the number is odd");
+}
+return(0);
+}
+}
