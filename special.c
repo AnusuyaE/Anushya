b324e213ba3429dbae25296efbf92661d3e4df08
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2018-01-21 10:59:49 +0530

    Create special.c

---
+++ special.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char senten[30];
+int i,ct=1;
+scanf("%s",&senten);
+for(i=0;i<sentena[i]!=null;i++)
+{
+if(sentena[i]='@'||sentena[i]='#'||sentena[i]='$'||sentena[i]='&')
+{
+ct++;
+}
+}
+printf("%d",ct);
+}
