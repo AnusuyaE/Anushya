794230b7bc3f0cae93161cb719fb3eaa33fcec18
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2018-01-25 16:03:51 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,17 @@
+#includr<stdio.h>
+#incude<ctype.h>
+void main()
+{
+char a;
+printf("\n enter the character:");
+scanf("%c",&a);
+if(a>='0')&&(a=<'9'))
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+return 0;
+}
