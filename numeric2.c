7f96e70d09351cb65dfc6b95ad6735a22cf01228
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2018-01-25 16:25:03 +0530

    Create numeric2.c

---
+++ numeric2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+char a,b;
+printf("\n enter the character:");
+scanf("%c%c",&a,&b);
+if(a>='0'&&a<='9')
+{
+printf(\n yes");
+}
+else
+{
+printf("\n no");
+}
+return 0;
+}
