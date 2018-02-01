2d77f059801e57369ea012ad40a852a61eba3138
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2018-02-01 08:33:54 +0000

    Create swap2.c

---
+++ swap2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b,temp;
+printf("enter the swapping number");
+scanf("%d%d",&a,&b);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+temp=a;
+a=b;
+b=temp;
+printf("the swapping number"a,b);
+}
