c09a778bf0c5be1f8743c52f2c54a524b200fa94
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2018-02-02 14:28:04 +0530

    Create holiday.c

---
+++ holiday.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char a[100];
+printf("enter the day");
+scanf("%s",&a);
+if(a=='saturday'||a=='sunday')
+{
+printf("\n holiday");
+}
+else
+{
+printf("\n no holiday");
+}
+}
