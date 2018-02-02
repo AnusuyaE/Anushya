544ac1af837d641f739be42e08baaf638f64ebf3
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2018-02-02 14:16:20 +0530

    Rename numeric2.c to char.c

---
+++ char.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+intchar[10],j,i,a;
+int n;
+int count=0;
+for(j=0;j<12;j++)
+{
+printf(enter the string");
+scanf("%d",&i);
+for(a=o;a[i]!=0;n++)
+if(a[i]=0)
+{
+count++;
+}
+}
+}
