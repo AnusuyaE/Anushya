2d66bdec59e9fd92ee0a76a24db4c0f4f84b1b2d
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2018-03-20 16:23:00 +0530

    Create numdigit.c

---
+++ numdigit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[10];
+    int b[10],i;
+    scanf("%s",&a[i]);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]>=0&&a[i]<=9)
+        {
+            printf("%d",b[i]);
+        }
+    }
+    return 0;
+    
+}
